aad0841cdf2d78f20999f83e68e825f023d704e3
joohyeon98 <ljh221803@gmail.com>
2024-10-28 23:31:19 +0900

    todo

--- src/main/java/com/nhnacademy/jdbc/student/repository/impl/StatementStudentRepository.java
+++ src/main/java/com/nhnacademy/jdbc/student/repository/impl/StatementStudentRepository.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 @Slf4j
 public class StatementStudentRepository implements StudentRepository {
 
+    String sql = new Studnet(String id, )
     @Override
     public int save(Student student){
         //todo#1 insert student
