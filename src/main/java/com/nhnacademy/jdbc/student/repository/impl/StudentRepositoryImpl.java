dff4759a92ce772c303a9b5561f36157738cd44d
joohyeon98 <ljh221803@gmail.com>
2024-10-29 16:30:54 +0900

    todo

--- src/main/java/com/nhnacademy/jdbc/student/repository/impl/StudentRepositoryImpl.java
+++ src/main/java/com/nhnacademy/jdbc/student/repository/impl/StudentRepositoryImpl.java
@@ -123,13 +123,69 @@ public class StudentRepositoryImpl implements StudentRepository {
     @Override
     public long totalCount(Connection connection) {
         //todo#4 totalCount 구현
-        return 0l;
+        String sql = "select count(*) from jdbc_students";
+        ResultSet rs = null;
+
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            rs = statement.executeQuery();
+            if (rs.next()) {
+                return rs.getLong(1);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (Objects.nonNull(rs)) {
+                    rs.close();
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return 0L;
     }
 
     @Override
     public Page<Student> findAll(Connection connection, int page, int pageSize) {
         //todo#5 페이징 처리 구현
-        return null;
-    }
+        int offset = (page - 1) * pageSize;
+        int limit = pageSize;
 
+        ResultSet rs = null;
+        String sql = "select id, name, gender, age, created_at from jdbc_students order by id desc limit ?,?";
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, offset);
+            statement.setInt(2, limit);
+            rs = statement.executeQuery();
+            List<Student> studentList = new ArrayList<>(pageSize);
+
+            while (rs.next()) {
+                studentList.add(
+                        new Student(
+                                rs.getString("id"),
+                                rs.getString("name"),
+                                Student.GENDER.valueOf(rs.getString("gender")),
+                                rs.getInt("age"),
+                                rs.getTimestamp("created_at").toLocalDateTime()
+                        )
+                );
+            }
+            long total = 0;
+            if (!studentList.isEmpty()) {
+                // size>0 조회 시도, 0이면 조회할 필요 없음, count query는 자원을 많이 소모함
+                total = totalCount(connection);
+            }
+            return new Page<Student>(studentList, total);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (Objects.nonNull(rs)) {
+                    rs.close();
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 }
